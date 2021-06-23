e50fc95472594bda3d16bfbf6d8ea65059dab810
Flávio Paixão <flavio_pc@outlook.com>
2021-06-23 10:04:20 -0300

    add google signin

---
+++ lib/modules/login/login_controller.dart
@@ -0,0 +1,18 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginController {
+  Future<void> googleSignIn() async {
+    GoogleSignIn _googleSignIn = GoogleSignIn(
+      scopes: [
+        'email',
+      ],
+    );
+
+    try {
+      final response = await _googleSignIn.signIn();
+      print(response);
+    } catch (error) {
+      print(error);
+    }
+  }
+}
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +14,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -54,9 +57,7 @@ class _LoginPageState extends State<LoginPage> {
                     padding:
                         const EdgeInsets.only(right: 40, left: 40, top: 10),
                     child: SocialLoginButton(
-                      onTap: () {
-                        print("click");
-                      },
+                      onTap: controller.googleSignIn,
                     ),
                   )
                 ],
