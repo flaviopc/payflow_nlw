4a232fc4181d4194ba3807034d0ea436e715eb54
Flávio Paixão <flavio_pc@outlook.com>
2021-06-25 10:48:30 -0300

    criação da lista de boletos

---
+++ lib/shared/widgets/boleto_list/boleto_list_controller.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:payflow/shared/models/boleto_model.dart';
+
+class BoletoListController {
+  final boletosNotifier = ValueNotifier<List<BoletoModel>>(<BoletoModel>[]);
+  List<BoletoModel> get boletos => boletosNotifier.value;
+  set boletos(List<BoletoModel> value) => boletosNotifier.value = value;
+  BoletoListController() {
+    getBoletos();
+  }
+
+  Future<void> getBoletos() async {
+    try {
+      final instance = await SharedPreferences.getInstance();
+      final response = instance.getStringList("boletos") ?? <String>[];
+      boletos = response.map((e) => BoletoModel.fromJson(e)).toList();
+    } catch (e) {
+      boletos = <BoletoModel>[];
+    }
+  }
+}
---
+++ lib/shared/widgets/boleto_list/boleto_list_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/boleto_model.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list_controller.dart';
+import 'package:payflow/shared/widgets/boleto_tile/boleto_tile_widget.dart';
+
+class BoletoListWidget extends StatefulWidget {
+  const BoletoListWidget({Key? key}) : super(key: key);
+
+  @override
+  _BoletoListWidgetState createState() => _BoletoListWidgetState();
+}
+
+class _BoletoListWidgetState extends State<BoletoListWidget> {
+  final controller = BoletoListController();
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<List<BoletoModel>>(
+      valueListenable: controller.boletosNotifier,
+      builder: (_, boletos, __) => Column(
+        children: boletos.map((e) => BoletoTileWidget(data: e)).toList(),
+      ),
+    );
+  }
+}
